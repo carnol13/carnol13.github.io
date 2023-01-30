f677f19a1ebc8edb791e28f115d22c5667cd5478
carnol13 <104369640+carnol13@users.noreply.github.com>
2023-01-30 15:35:55 -0500

    Update checkoutForm.js

--- checkoutForm.js
+++ checkoutForm.js
@@ -6,7 +6,15 @@ var checkOutDate = document.getElementById("checkoutdate");
 var returnDate = document.getElementById("returndate");
 var checkOutTime = document.getElementById("checkOutTime");
 var returnTime = document.getElementById("Return Time");
-var equipment = document.getElementById("equipment");
+var equipment1 = document.getElementById("equipment1");
+var equipment2 = document.getElementById("equipment2");
+var equipment3 = document.getElementById("equipment3");
+var equipment4 = document.getElementById("equipment4");
+var equipment5 = document.getElementById("equipment5");
+var equipment6 = document.getElementById("equipment6");
+var equipment7 = document.getElementById("equipment7");
+var equipment8 = document.getElementById("equipment8");
+
 var form = document.getElementById("CheckOutForm");
 
 function afterSubmit(e){
@@ -21,7 +29,15 @@ function afterSubmit(e){
         returnDate: returnDate.value,
         checkOutTime: checkOutTime.value,
         returnTime: returnDate.value,
-        equipment: equipment.value
+        equipment1: equipment1.value,
+        equipment2: equipment2.value,
+        equipment3: equipment3.value,
+        equipment4: equipment4.value,
+        equipment5: equipment5.value,
+        equipment6: equipment6.value,
+        equipment7: equipment7.value,
+        equipment8: equipment8.value,
+
     };
 
     
